a0ded5982aa9d3fd148c00e77a0c8e00ad0fe59c
Pedro Pena <pedrohenrique.pena@outlook.com>
2023-02-24 20:58:51 -0300

    fatiando string

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 
-url = "https://bytebank.com/cambio?moedaOrigme=real&moedaDestino=dolar&quantidade=100"
-print(url)
+url                 = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+indice_interrogacao = url.find("?")
+url_base            = url[ :indice_interrogacao]
+url_parametros      = url[indice_interrogacao + 1 : ]
+parametro_de_busca  = "moedaOrigem"
+indice_parametro    = url_parametros.find(parametro_de_busca)
+tamanho_parametro   = len(parametro_de_busca)
+indice_valor        = indice_parametro + tamanho_parametro + 1
+indice_e_comercial  = url_parametros.find("&")
+valor               = url_parametros[indice_valor : indice_e_comercial ]
+print(valor)
+
+
+
+
+
+
+
+
+
+
