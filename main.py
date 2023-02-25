dd56c29cf8f0366dd19c08aa3fa8acc604bc7e4c
Pedro Pena <pedrohenrique.pena@outlook.com>
2023-02-24 21:15:08 -0300

    parametro de busca

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 
-url                 = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+url = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+
+
+#Separa a base e os parâmentros 
 indice_interrogacao = url.find("?")
 url_base            = url[ :indice_interrogacao]
 url_parametros      = url[indice_interrogacao + 1 : ]
+
+
+#Busca o valor de um parâmetro
 parametro_de_busca  = "moedaOrigem"
 indice_parametro    = url_parametros.find(parametro_de_busca)
 tamanho_parametro   = len(parametro_de_busca)
 indice_valor        = indice_parametro + tamanho_parametro + 1
-indice_e_comercial  = url_parametros.find("&")
-valor               = url_parametros[indice_valor : indice_e_comercial ]
+indice_e_comercial  = url_parametros.find("&", indice_valor)
+
+if indice_e_comercial == -1:
+   valor  = url_parametros[indice_valor : ]
+else: 
+   valor = url_parametros[indice_valor : indice_e_comercial]
+
+
 print(valor)
 
 
